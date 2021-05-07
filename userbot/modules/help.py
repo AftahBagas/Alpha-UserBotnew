2f50b388b61975bda32884d49bf1eaf57f721140
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-07 20:38:52 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -24,13 +24,37 @@ async def help(event):
             await asyncio.sleep(200)
             await event.delete()
     else:
-        string = ""
-        for i in CMD_HELP:
-            string += "`" + str(i)
-            string += "`\t🔥  "
-        await event.edit("**🔥 Daftar Perintah Untuk\n🔥A L P H A🔥:\n\n**"
-                         f"🔥{string}🔥"
-                         "\n 🔥 A L P H A 🔥")
-        await event.reply(f"\n**Ketik Contoh** `.help kata-kata` **Untuk Informasi Perintah**")
+        await Alpha.edit("Dᴀғᴛᴀʀ Pᴇʀɪɴᴛᴀʜ Uɴᴛᴜᴋ Aʟᴘʜᴀ 🍁")
+        await Alpha.edit("**🍁 MODULES 1:**\n"
+                        "admin  adzan  afk  gabut  vip  animasi  android  anime  anti_spambot  aria  ascii\n\n"
+                        "**🍁 MODULES 2:**\n"
+                        "blacklist  carbon   chat  mutechat  covid  membuat  deepfry  emojigames\n\n"
+                        "**🍁 MODULES 3:**\n"
+                        "eval  exec  term  fakegban  federations  figlet  filter  gban  gcast  gdrive  gcommit  github\n\n"
+                        "**🍁 MODULES 4:**\n"
+                        "glitch  gps  hash  base64  hentai  heroku  id  imgmeme  kekuatan\n\n"
+                        "**🍁 MODULES 5:**\n"
+                        "lastfm  locks  lord  aeshtetic  deteksi  alphafun  alphahelper  hazmat\n\n"
+                        "**🍁 MODULES 6:**\n"
+                        "instagram  amongus  Alphamemes  misc  app  undelete  grab  clone\n\n"
+                        "**🍁 MODULES 7:**\n"
+                        "randomprofil  song  tiny  tempmail  tiktok  wordcloud\n\n" 
+                        "**🍁 MODULES 8:**\n"
+                        "lyrics  mega  memes  memify  mentions  purge  purgeme  del  edit\n\n"
+                        "**🍁 MODULES 9:**\n"
+                        "sd  random  sleep  shutdown  repo  readme  repeat  restart\n\n"
+                        "**🍁 MODULES 10:**\n"
+                        "raw  nekobot  notes  off  phreaker  pm  profil  quotly  rastick  resi  reverse  salam  sangmata\n\n"
+                        "**🍁 MODULES 11:**\n"
+                        "santetonline  image_search  currency  google  wiki  ud  tts  translate  youtube  rip\n\n"
+                        "**🍁 MODULES 12:**\n"
+                        "removebg  ocr  qrcode  barcode  paste  getpaste  nekobin  direct  screenshot  sed  snips  spam  spotifynow  ssvideo\n\n"
+                        "**🍁 MODULES 13:**\n"
+                        "stickers  stickers2  sysd  botver  pip  alive  tag_all  telegraph  timedate  torrent\n\n" 
+                        "**🍁 MODULES 14:**\n"
+                        "transform  update  download  getid  waifu  wallpaper  weather\n\n"
+                        "**🍁 MODULES 15:**\n"
+                        "webupload  welcome  whois  ping  sinyal  xiaomi  zipfile")
+        await Alpha.reply("\n**Cᴏɴᴛᴏʜ:**\n**Kᴇᴛɪᴋ** `.help kata kata` **Uɴᴛᴜᴋ Iɴғᴏʀᴍᴀsɪ Pᴇʀɪɴᴛᴀʜ**")
         await asyncio.sleep(1000)
-        await event.delete()
+        await Alpha.delete()
