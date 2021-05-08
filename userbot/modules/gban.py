46ad5d28b2f775d1eca52fcf14b26bc8799b4557
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-08 19:58:19 +0700

    Update and rename gban_sql.py to gban.py

--- userbot/modules/gban.py
+++ userbot/modules/gban.py
@@ -1,36 +1,10 @@
-@register(incoming=True)
-async def muter(moot):
-    try:
-        from userbot.modules.sql_helper.gban_sql import is_gbanned
-        from userbot.modules.sql_helper.spam_ban_sql import is_banned
-    except AttributeError:
-        return
-    banned = is_banned(moot.chat_id)
-    gbanned = is_gbanned(moot.sender_id)
-    rights = ChatBannedRights(
-        until_date=None,
-        send_messages=True,
-        send_media=True,
-        send_stickers=True,
-        send_gifs=True,
-        send_games=True,
-        send_inline=True,
-        embed_links=True,
-    )
-    if banned:
-        for i in banned:
-            if str(i.sender) == str(moot.sender_id):
-                await moot.delete()
-                await moot.client(
-                    EditBannedRequest(moot.chat_id, moot.sender_id, rights)
-                )
-    for i in gbanned:
+for i in gbanned:
         if i.sender == str(moot.sender_id):
             await moot.delete()
 
 
 @register(outgoing=True, pattern=r"^\.ungban(?: |$)(.*)")
-async def ungmoot(un_gban):
+async def ungbon(un_gban):
     # Admin or creator check
     chat = await un_gban.get_chat()
     admin = chat.admin_rights
@@ -44,21 +18,21 @@ async def ungmoot(un_gban):
     try:
         from userbot.modules.sql_helper.gban_sql import ungban
     except AttributeError:
-        return await un_gmute.edit(NO_SQL)
+        return await un_gban.edit(NO_SQL)
 
     user = await get_user_from_event(un_gban)
     user = user[0]
     if not user:
         return
 
-    # If pass, inform and start ungmuting
-    await un_gban.edit("```Menjalankan Global Banned ! ```")
+    # If pass, inform and start ungbanned
+    await un_gban.edit("```Menjalankan Global Blokir ! ```")
 
     if ungban(user.id) is False:
         await un_gban.edit("`Kesalahan! Pengguna Sedang Tidak Di Gban.`")
     else:
         # Inform about success
-        await un_gban.edit("```Berhasil! Pengguna Sudah Tidak Lagi Dibanned```")
+        await un_gban.edit("```Berhasil! Pengguna Sudah Tidak Lagi Diblokir```")
         await sleep(3)
         await un_gban.delete()
 
@@ -93,14 +67,14 @@ async def gspider(gspdr):
         return
 
     # If pass, inform and start gbaning
-    await gspdr.edit("`Berhasil Membanned Pengguna!`")
+    await gspdr.edit("`Berhasil Memblokir Pengguna!`")
     if gban(user.id) is False:
-        await gspdr.edit("`Kesalahan! Pengguna Sudah Dibanned.`")
+        await gspdr.edit("`Kesalahan! Pengguna Sudah Diblokir.`")
     else:
         if reason:
-            await gspdr.edit(f"**Dibanned Disemua Media Lord Java!**\n**Alasan:** `{reason}`")
+            await gspdr.edit(f"**Dibisukan Disemua Media Alpha!**\n**Alasan:** `{reason}`")
         else:
-            await gspdr.edit("`Berhasil Banned Pengguna Secara Global!`")
+            await gspdr.edit("`Berhasil Memblokir Pengguna Secara Global!`")
 
         if BOTLOG:
             await gspdr.client.send_message(
