11b8a467d769d2385aea564d549cb0ddb58c889f
Zora24 <liualvinas1212@gmail.com>
2021-05-08 03:30:57 +0000

    pylint: auto fixes
    Signed-off-by: Zora24 <liualvinas1212@gmail.com>

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -112,6 +112,7 @@ async def pingme(pong):
                     f"`{uptime}` \n"
                     f"┖━─━───────" % (duration))
 
+
 @register(outgoing=True, pattern="^.sinyal$")
 async def pingme(pong):
     """ For .ping command, ping the userbot from any chat.  """
