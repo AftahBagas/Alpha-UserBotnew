c4900a3b09bd2a5220331582f8fca4fc03220885
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-12 23:37:37 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -455,7 +455,7 @@ with bot:
         @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
         async def on_plug_in_callback_query_handler(event):
             if event.query.user_id == bot.uid:
-            await event.edit("Mᴇɴᴜ Tᴇʟᴀʜ Dɪᴛᴜᴛᴜᴘ")
+                await event.edit("Mᴇɴᴜ Tᴇʟᴀʜ Dɪᴛᴜᴛᴜᴘ")
             else:
                 reply_pop_up_alert = f"Harap Deploy Alpha Anda Sendiri, Jangan Menggunakan Milik {ALIVE_NAME} ツ"
                 await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
