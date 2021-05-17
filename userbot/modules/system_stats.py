c28dbe11b53750875565e256ea1defe2e6cf770f
Zora24 <liualvinas1212@gmail.com>
2021-05-17 16:13:59 +0000

    pylint: auto fixes
    Signed-off-by: Zora24 <liualvinas1212@gmail.com>

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -255,7 +255,7 @@ async def amireallyalive(alive):
 
 @register(outgoing=True, pattern=r"^\.(?:xalive|xon)\s?(.)?")
 async def amireallyalive(alive):
-    user = await bot.get_me()
+    await bot.get_me()
     await get_readable_time((time.time() - StartTime))
     output = (
         f"**The Alpha Userbot...** \n"
