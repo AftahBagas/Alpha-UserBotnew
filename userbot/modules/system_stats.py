b243711d0cf587c4f881148106c5c8c7f159a39d
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-18 06:38:35 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -226,12 +226,12 @@ async def amireallyalive(alive):
         f"**The Alpha Userbot...**\n\n"
         f"**Hey, I am alive.\n\n"
         f"┏━━━━━━━━━━━━━━━━━━━━━━━\n"
-        f"┣ **Pemilik** - `{DEFAULTUSER}` \n"
-        f"┣ **Versi** - `{BOT_VER}` \n"
-        f"┣ **UpTime** - `{uptime}`" % (duration)` \n
-        f"┣ **Python** - `Ver {python_version()}` \n"
-        f"┣ **Telethon** - `{version.__version__}` \n"
-        f"┣ **Branch** - `{UPSTREAM_REPO_BRANCH}` \n"
+        f"┣**Pemilik** - `{DEFAULTUSER}` \n"
+        f"┣**Versi** - `{BOT_VER}` \n"
+        f"┣**UpTime** - `{uptime}`" % (duration)` \n
+        f"┣**Python** - `Ver {python_version()}` \n"
+        f"┣**Telethon** - `{version.__version__}` \n"
+        f"┣**Branch** - `{UPSTREAM_REPO_BRANCH}` \n"
         f"┗━━━━━━━━━━━━━━━━━━━━━━━")
     if ALIVE_LOGO:
         try:
