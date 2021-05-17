63566f712fe49b937fdb36456b6d78de2d68f57c
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-17 22:50:19 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -1,4 +1,4 @@
-# System Stats Lord-Userbot
+
 
 import asyncio
 from asyncio import create_subprocess_exec as asyncrunapp
@@ -220,7 +220,7 @@ async def pipcheck(pip):
 
 @register(outgoing=True, pattern=r"^\.(?:alpha|alphaon)\s?(.)?")
 async def amireallyalive(alive):
-    await bot.get_me()
+    user = await bot.get_me()
     await get_readable_time((time.time() - StartTime))
     output = (
         f"**The Alpha Userbot...** \n"
