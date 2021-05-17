8a092910d47bd817b03454ad5a5ebdf942c04076
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-17 23:36:54 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -255,13 +255,11 @@ async def amireallyalive(alive):
 
 @register(outgoing=True, pattern=r"^\.(?:xalive|xon)\s?(.)?")
 async def amireallyalive(alive):
-    await bot.get_me()
+    user = await bot.get_me()
     await get_readable_time((time.time() - StartTime))
     output = (
-        f"**The Alpha Userbot...** \n"
-        f"\n"
-        f"**Hey, I am alive.**\n"
-        f"\n"
+        f"**The Alpha Userbot...**\n\n"
+        f"**Hey, I am alive.\n\n"
         f"┏━━━━━━━━━━━━━━━━━━━━━━━\n"
         f"┣**Pemilik** - `{DEFAULTUSER}` \n"
         f"┣**Versi** - `{BOT_VER}` \n"
