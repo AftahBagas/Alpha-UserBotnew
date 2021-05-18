a44e46a3ecb9cfecb1557bb8433deb0275f8623b
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-18 14:46:46 +0700

    Update heroku.py

--- userbot/modules/heroku.py
+++ userbot/modules/heroku.py
@@ -183,7 +183,7 @@ async def dyno_usage(dyno):
                 f" 🌸 **Pemakaian Dyno {app.name} :**\n"
                 f"• **Pemakaian** :  `{AppHours}` **Jam** - `{AppMinutes}` **Menit**\n"
                 f"• **Persen** : `{AppPercentage}`**%**\n"
-                "•◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛• \n"
+                "┃◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛┃ \n"
                 f" 🌸 **Sisa Dyno Ini Bulan :**\n"
                 f"• **Sisa** :  `{hours}` **Jam** - `{minutes}` **Menit**\n"
                 f"• **Persen** :  `{percentage}`**%**\n"
