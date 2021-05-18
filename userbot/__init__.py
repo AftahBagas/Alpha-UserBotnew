70107e2e550c2c105e1c7e1bb2de8e3184356e1b
Zora24 <liualvinas1212@gmail.com>
2021-05-18 06:28:46 +0000

    pylint: auto fixes
    Signed-off-by: Zora24 <liualvinas1212@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -351,7 +351,7 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = sorted(helpable_modules)
     modules = [
         custom.Button.inline("{} {} •".format(
-        "•", x), data="ub_modul_{}".format(x))
+            "•", x), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
