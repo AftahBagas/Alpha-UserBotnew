81ebfb5ffb1d7abd4c3471447e145cbfe1875620
Zora24 <liualvinas1212@gmail.com>
2021-05-18 05:56:53 +0000

    pylint: auto fixes
    Signed-off-by: Zora24 <liualvinas1212@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -350,7 +350,9 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline("{} {} •".format("•", x), data="ub_modul_{}".format(x))
+        custom.Button.inline(
+    "{} {} •".format(
+        "•", x), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
@@ -421,7 +423,7 @@ with bot:
             else:
                 result = builder.article(
                     "**Aʟᴘʜᴀ**",
-                    text="**Aʟᴘʜᴀ Usᴇʀʙᴏᴛ**\n**Bᴜᴀᴛ Usᴇʀʙᴏᴛ Aɴᴅᴀ Sᴇɴᴅɪʀɪ Dᴇɴɢᴀɴ**", [Tekan Disini](t.me/teamsquaduserbotsupport)""",
+                    text="**Aʟᴘʜᴀ Usᴇʀʙᴏᴛ**\n**Bᴜᴀᴛ Usᴇʀʙᴏᴛ Aɴᴅᴀ Sᴇɴᴅɪʀɪ Dᴇɴɢᴀɴ**", [Tekan Disini](t.me / teamsquaduserbotsupport)""",
                     buttons=[
                         [
                             custom.Button.url(
