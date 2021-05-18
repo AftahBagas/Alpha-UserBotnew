a980e8b3959977aa73b1c9f7057efda56ca7b4cf
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-18 09:20:37 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -120,7 +120,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "**Alpha Userbot**\n• [[Repo Alpha]](https://github.com/AftahBagas/Alpha_)\n**• [[Group Support]](https://t.me/teamsquaduserbotsupport)**\n"
+        "**Alpha Userbot**\n• [ [Repo Alpha](https://github.com/AftahBagas/Alpha_) ]\n**• [ [Group Support](https://t.me/teamsquaduserbotsupport) ]**\n"
     )
 
 
