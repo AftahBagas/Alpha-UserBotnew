86a23b464ebe661fb7a6992b7a1a7f0372aa230d
Zora24 <liualvinas1212@gmail.com>
2021-05-19 04:25:06 +0000

    pylint: auto fixes
    Signed-off-by: Zora24 <liualvinas1212@gmail.com>

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -15,6 +15,7 @@ modules = CMD_HELP
 
 # ALPHA USERBOT
 
+
 @register(outgoing=True, pattern="^.help(?: |$)(.*)")
 async def help(alpha):
     """ For .help command,"""
@@ -29,35 +30,35 @@ async def help(alpha):
     else:
         await alpha.edit("✨")
         await alpha.edit("**✨ MODULES 1:**\n"
-                        "`admin`  `adzan`  `afk`  `gabut`  `vip`  `animasi`  `android`  `anime`  `anti_spambot`  `aria`  `ascii`\n\n"
-                        "**✨ MODULES 2:**\n"
-                        "`blacklist`  `carbon`   `chat`  `mutechat`  `covid`  `membuat`  `deepfry`  `emojigames`\n\n"
-                        "**✨ MODULES 3:**\n"
-                        "`eval`  `exec`  `term`  `fakegban`  `federations`  `figlet`  `filter`  `gban`  `gcast`  `gdrive`  `gcommit`  `github`\n\n"
-                        "**✨ MODULES 4:**\n"
-                        "`glitch`  `gps`  `hash`  `base64`  `hentai`  `heroku`  `id`  `imgmeme`  `kekuatan`\n\n"
-                        "**✨ MODULES 5:**\n"
-                        "`lastfm`  `locks`  `alpha`  `aeshtetic`  `deteksi`  `alphafun`  `alphahelper`  `hazmat`\n\n"
-                        "**✨ MODULES 6:**\n"
-                        "`instagram`  `amongus`  `alphamemes`  `misc`  `app`  `undelete`  `grab`  `clone`\n\n"
-                        "**✨ MODULES 7:**\n"
-                        "`randomprofil`  `song`  `tiny`  `tempmail`  `tiktok`  `wordcloud`\n\n" 
-                        "**✨ MODULES 8:**\n"
-                        "`lyrics`  `mega`  `memes`  `memify`  `mentions`  `purge`  `purgeme`  `del`  `edit`\n\n"
-                        "**✨ MODULES 9:**\n"
-                        "`sd`  `random`  `sleep`  `shutdown`  `repo`  `readme`  `repeat`  `restart`\n\n"
-                        "**✨ MODULES 10:**\n"
-                        "`raw`  `nekobot`  `notes`  `off`  `phreaker`  `pm`  `profil`  `quotly`  `rastick`  `resi`  `reverse`  `salam`  `sangmata`\n\n"
-                        "**✨ MODULES 11:**\n"
-                        "`santetonline`  `image_search`  `currency`  `google`  `wiki`  `ud`  `tts`  `translate`  `youtube`  `rip`\n\n"
-                        "**✨ MODULES 12:**\n"
-                        "`removebg`  `ocr`  `qrcode`  `barcode`  `paste`  `getpaste`  `nekobin`  `direct`  `screenshot`  `sed`  `snips`  `spam`  `spotifynow`  `ssvideo`\n\n"
-                        "**✨ MODULES 13:**\n"
-                        "`stickers`  `stickers2`  `sysd`  `botver`  `pip`  `alive`  `tag_all`  `telegraph`  `timedate`  `torrent`\n\n" 
-                        "**✨ MODULES 14:**\n"
-                        "`transform`  `update`  `download`  `getid`  `waifu`  `wallpaper`  `weather`\n\n"
-                        "**✨ MODULES 15:**\n"
-                        "`webupload`  `welcome`  `whois`  `ping`  `sinyal`  `xiaomi`  `zipfile`")
+                         "`admin`  `adzan`  `afk`  `gabut`  `vip`  `animasi`  `android`  `anime`  `anti_spambot`  `aria`  `ascii`\n\n"
+                         "**✨ MODULES 2:**\n"
+                         "`blacklist`  `carbon`   `chat`  `mutechat`  `covid`  `membuat`  `deepfry`  `emojigames`\n\n"
+                         "**✨ MODULES 3:**\n"
+                         "`eval`  `exec`  `term`  `fakegban`  `federations`  `figlet`  `filter`  `gban`  `gcast`  `gdrive`  `gcommit`  `github`\n\n"
+                         "**✨ MODULES 4:**\n"
+                         "`glitch`  `gps`  `hash`  `base64`  `hentai`  `heroku`  `id`  `imgmeme`  `kekuatan`\n\n"
+                         "**✨ MODULES 5:**\n"
+                         "`lastfm`  `locks`  `alpha`  `aeshtetic`  `deteksi`  `alphafun`  `alphahelper`  `hazmat`\n\n"
+                         "**✨ MODULES 6:**\n"
+                         "`instagram`  `amongus`  `alphamemes`  `misc`  `app`  `undelete`  `grab`  `clone`\n\n"
+                         "**✨ MODULES 7:**\n"
+                         "`randomprofil`  `song`  `tiny`  `tempmail`  `tiktok`  `wordcloud`\n\n"
+                         "**✨ MODULES 8:**\n"
+                         "`lyrics`  `mega`  `memes`  `memify`  `mentions`  `purge`  `purgeme`  `del`  `edit`\n\n"
+                         "**✨ MODULES 9:**\n"
+                         "`sd`  `random`  `sleep`  `shutdown`  `repo`  `readme`  `repeat`  `restart`\n\n"
+                         "**✨ MODULES 10:**\n"
+                         "`raw`  `nekobot`  `notes`  `off`  `phreaker`  `pm`  `profil`  `quotly`  `rastick`  `resi`  `reverse`  `salam`  `sangmata`\n\n"
+                         "**✨ MODULES 11:**\n"
+                         "`santetonline`  `image_search`  `currency`  `google`  `wiki`  `ud`  `tts`  `translate`  `youtube`  `rip`\n\n"
+                         "**✨ MODULES 12:**\n"
+                         "`removebg`  `ocr`  `qrcode`  `barcode`  `paste`  `getpaste`  `nekobin`  `direct`  `screenshot`  `sed`  `snips`  `spam`  `spotifynow`  `ssvideo`\n\n"
+                         "**✨ MODULES 13:**\n"
+                         "`stickers`  `stickers2`  `sysd`  `botver`  `pip`  `alive`  `tag_all`  `telegraph`  `timedate`  `torrent`\n\n"
+                         "**✨ MODULES 14:**\n"
+                         "`transform`  `update`  `download`  `getid`  `waifu`  `wallpaper`  `weather`\n\n"
+                         "**✨ MODULES 15:**\n"
+                         "`webupload`  `welcome`  `whois`  `ping`  `sinyal`  `xiaomi`  `zipfile`")
         await alpha.reply("\n**CARA MENGGUNAKAN,** **CONTOH:**\n**KETIK** `.help afk` **UNTUK INFORMASI MODULES**\n**GROUP SUPPORT:** [TEKAN](t.me/teamsquaduserbotsupport)")
         await asyncio.sleep(1000)
         await alpha.delete()
