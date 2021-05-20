2cd5ed55bc359cd57dc606c9e8cb87787a230578
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-20 11:38:54 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -3,9 +3,7 @@
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
-""" Alpha Userbot help command """
-
-# TEAM MASTER USERBOT
+""" Userbot help command """
 
 import asyncio
 from userbot import CMD_HELP
@@ -13,52 +11,25 @@ from userbot.events import register
 
 modules = CMD_HELP
 
-# ALPHA USERBOT
-
 
 @register(outgoing=True, pattern="^.help(?: |$)(.*)")
-async def help(alpha):
+async def help(event):
     """ For .help command,"""
-    args = alpha.pattern_match.group(1).lower()
+    args = event.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
-            await alpha.edit(str(CMD_HELP[args]))
+            await event.edit(str(CMD_HELP[args]))
         else:
-            await alpha.edit("**Maaf alpha, Saya Tidak Punya Perintah Itu ツ**")
+            await event.edit("**Module Yg Lu Cari Gada Gblk**")
             await asyncio.sleep(200)
-            await alpha.delete()
+            await event.delete()
     else:
-        await alpha.edit("✨")
-        await alpha.edit("**✨ MODULES 1:**\n"
-                         "`admin` - `adzan` - `afk` - `gabut` - `vip` - `animasi` - `android` - `anime` - `anti_spambot` - `aria` - `ascii`\n\n"
-                         "**✨ MODULES 2:**\n"
-                         "`blacklist` - `carbon`  - `chat` - `mutechat` - `covid` - `membuat` - `deepfry` - `emojigames`\n\n"
-                         "**✨ MODULES 3:**\n"
-                         "`eval` - `exec` - `term` - `fakegban` - `federations` - `figlet` - `filter` - `gban` - `gcast` - `gdrive` - `gcommit` - `github`\n\n"
-                         "**✨ MODULES 4:**\n"
-                         "`glitch` - `gps`  `hash` - `base64` - `hentai` - `heroku` - `id` - `imgmeme` - `kekuatan`\n\n"
-                         "**✨ MODULES 5:**\n"
-                         "`lastfm` - `locks` - `alpha` - `aeshtetic` - `deteksi` - `alphafun` - `alphahelper` - `hazmat`\n\n"
-                         "**✨ MODULES 6:**\n"
-                         "`instagram` - `amongus` - `alphamemes` - `misc` - `app` - `undelete` - `grab` - `clone`\n\n"
-                         "**✨ MODULES 7:**\n"
-                         "`randomprofil` - `song` - `tiny` - `tempmail` - `tiktok` - `wordcloud`\n\n"
-                         "**✨ MODULES 8:**\n"
-                         "`lyrics` - `mega`  `memes` - `memify` - `mentions` - `purge` - `purgeme` - `del` - `edit`\n\n"
-                         "**✨ MODULES 9:**\n"
-                         "`sd` - `random`  `sleep` - `shutdown` - `repo` - `readme` - `repeat` - `restart`\n\n"
-                         "**✨ MODULES 10:**\n"
-                         "`raw` - `nekobot` - `notes` - `off` - `phreaker` - `pm` - `profil` - `quotly` - `rastick` - `resi` - `reverse` - `salam` - `sangmata`\n\n"
-                         "**✨ MODULES 11:**\n"
-                         "`santetonline` - `image_search` - `currency` - `google` - `wiki` - `ud` - `tts` - `translate` - `youtube` - `rip`\n\n"
-                         "**✨ MODULES 12:**\n"
-                         "`removebg` - `ocr` - `qrcode` - `barcode` - `paste` - `getpaste` - `nekobin` - `direct`  `screenshot` - `sed` - `snips` - `spam` - `spotifynow` - `ssvideo`\n\n"
-                         "**✨ MODULES 13:**\n"
-                         "`stickers` - `stickers2` - `sysd` - `botver` - `pip`  `alive` - `tag_all` - `telegraph` - `timedate` - `torrent`\n\n"
-                         "**✨ MODULES 14:**\n"
-                         "`transform` - `update` - `download` - `getid` - `waifu` - `wallpaper` - `weather`\n\n"
-                         "**✨ MODULES 15:**\n"
-                         "`webupload` - `welcome` - `whois` - `ping` - `sinyal` - `xiaomi` - `zipfile`")
-        await alpha.reply("\n**CARA MENGGUNAKAN,** **CONTOH:**\n**KETIK** `.help afk` **UNTUK INFORMASI MODULES**")
+        string = ""
+        for i in CMD_HELP:
+            string += "`" + str(i)
+            string += "`\t⌬  "
+        await event.edit("**⚡𝘈𝘭𝘱𝘩𝘢⚡ Userbot\nDaftar Perintah Alpha:\n\n**"
+                         f"⌬{string}⌬\n")
+        await event.reply(f"\n**Ketik Contoh** `.help afk` **Untuk Informasi Perintah**")
         await asyncio.sleep(1000)
-        await alpha.delete()
+        await event.delete()
