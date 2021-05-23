1b25981fc72b09dbc0424de1d4d8b0ccfaac1da0
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-24 06:47:19 +0700

    Update Tini.py

--- userbot/modules/Tini.py
+++ userbot/modules/Tini.py
@@ -1,10 +1,4 @@
-# Port By Ilham/Dara For Petercord Userbot From Ultroid, Thanks
-# Based Plugins
-# Fixed By ManusiaRakitann/Koala
-# Dont Remove
-
-
-# TENTANG AKU DAN DIA
+# AlphaUserbot
 
 from PIL import Image
 import os
@@ -18,18 +12,18 @@ from userbot import CMD_HELP, bot
 async def ultiny(event):
     reply = await event.get_reply_message()
     if not (reply and (reply.media)):
-        await event.edit("`Alpha User, Mohon Balas Ke Sticker ,Tiny,Tono,Dongo😶`")
+        await event.edit("`Alpha User, Mohon Balas Ke Sticker Untuk Menjalankan Perintah`")
         return
     xx = await event.edit("`Memproses Tiny....`")
     ik = await bot.download_media(reply)
-    im1 = Image.open("AlphaUserbot/alphauserbot.png")
+    im1 = Image.open("userbot/AlphaUserbot.png")
     if ik.endswith(".tgs"):
         await event.client.download_media(reply, "ult.tgs")
         os.system("lottie_convert.py ult.tgs json.json")
         json = open("json.json", "r")
         jsn = json.read()
         json.close()
-        jsn = jsn.replace("512", "2000")
+        jsn = jsn.replace("512", "2500")
         open("json.json", "w").write(jsn)
         os.system("lottie_convert.py json.json ult.tgs")
         file = "ult.tgs"
@@ -42,20 +36,20 @@ async def ultiny(event):
         im = Image.open(fil)
         z, d = im.size
         if z == d:
-            xxx, yyy = 200, 200
+            xxx, yyy = 300, 300
         else:
             t = z + d
             a = z / t
             b = d / t
-            aa = (a * 100) - 50
-            bb = (b * 100) - 50
-            xxx = 200 + 5 * aa
-            yyy = 200 + 5 * bb
+            aa = (a * 100) - 150
+            bb = (b * 100) - 150
+            xxx = 300 + 5 * aa
+            yyy = 300 + 5 * bb
         k = im.resize((int(xxx), int(yyy)))
         k.save("k.png", format="PNG", optimize=True)
         im2 = Image.open("k.png")
         back_im = im1.copy()
-        back_im.paste(im2, (150, 0))
+        back_im.paste(im2, (200, 0))
         back_im.save("o.webp", "WEBP", quality=95)
         file = "o.webp"
         os.remove(fil)
@@ -64,20 +58,20 @@ async def ultiny(event):
         im = Image.open(ik)
         z, d = im.size
         if z == d:
-            xxx, yyy = 200, 200
+            xxx, yyy = 300, 300
         else:
             t = z + d
             a = z / t
             b = d / t
-            aa = (a * 100) - 50
-            bb = (b * 100) - 50
-            xxx = 200 + 5 * aa
-            yyy = 200 + 5 * bb
+            aa = (a * 100) - 150
+            bb = (b * 100) - 150
+            xxx = 300 + 5 * aa
+            yyy = 300 + 5 * bb
         k = im.resize((int(xxx), int(yyy)))
         k.save("k.png", format="PNG", optimize=True)
         im2 = Image.open("k.png")
         back_im = im1.copy()
-        back_im.paste(im2, (150, 0))
+        back_im.paste(im2, (200, 0))
         back_im.save("o.webp", "WEBP", quality=95)
         file = "o.webp"
         os.remove("k.png")
@@ -90,4 +84,4 @@ async def ultiny(event):
 # Petercord - Userbot
 
 CMD_HELP.update({"tini": "`.tini`\
-    \nPenjelasan: Untuk Memperkecil Sticker."})
+    \nPenjelasan: cek aja njir."})
