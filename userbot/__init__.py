9681146aa7333b9728b06a1c774b7a0dc8648522
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-24 06:59:27 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -350,8 +350,7 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline("{} {} ⌬".format(
-            "⌬", x), data="ub_modul_{}".format(x))
+        custom.Button.inline("{} {} ⌬".format("⌬", x), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
@@ -393,7 +392,7 @@ with bot:
         @tgbot.on(events.NewMessage(pattern="/start"))
         async def handler(event):
             if event.message.from_id != uid:
-                await event.reply("Alpha, Buat Userbot Mu Sendiri [Tekan Disini](https://github.com/Aftahbagas/Alpha.git)")
+                await event.reply("Alpha, Buat Userbot Mu Sendiri [Tekan Disini](https://github.com/Aftahbagas/Alpha_.git)")
             else:
                 await event.reply(f"`Hai {ALIVE_NAME}\n\nApa Kabarmu?`")
 
