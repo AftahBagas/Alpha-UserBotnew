36234812334c4daf447f2b79808828e9e762bf04
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-24 06:05:15 +0700

    Create pcast.py

---
+++ userbot/modules/pcast.py
@@ -0,0 +1,32 @@
+from userbot.events import register
+from userbot import CMD_HELP, bot
+
+
+@register(outgoing=True, pattern=r"^\.pcast(?: |$)(.*)")
+async def gucast(event):
+    xx = event.pattern_match.group(1)
+    if not xx:
+        return await event.edit("`Berikan beberapa teks untuk Siaran Global Personal`")
+    tt = event.text
+    msg = tt[7:]
+    kk = await event.edit("`Sedang Mengirim pesan persoalan secara global...`")
+    er = 0
+    done = 0
+    async for x in bot.iter_dialogs():
+        if x.is_user and not x.entity.bot:
+            chat = x.id
+            try:
+                done += 1
+                await bot.send_message(chat, msg)
+            except BaseException:
+                er += 1
+    await kk.edit(f"Berhasil Mengirim Pesan Ke `{done}` obrolan, kesalahan dalam `{er}` obrolan(s)")
+
+
+
+CMD_HELP.update(
+    {
+        "broadcast": "🍁𝘊𝘰𝘮𝘮𝘢𝘯𝘥: `.gcast`\
+         \nUsage : Mengirim Pesan persoalan Secara Global"
+    }
+)
