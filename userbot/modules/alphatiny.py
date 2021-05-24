471e11c702a8f5ffa703ff8696f28c011d7c91c1
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-24 08:32:08 +0700

    Update alphatiny.py

--- userbot/modules/alphatiny.py
+++ userbot/modules/alphatiny.py
@@ -15,7 +15,7 @@ async def _(event):
         return
     await event.edit("`Memperkecil Media...`")
     ik = await bot.download_media(reply)
-    im1 = Image.open("RAMUBOT.png")
+    im1 = Image.open("AlphaUserbot.png")
     if ik.endswith(".tgs"):
         await event.client.download_media(reply, "ult.tgs")
         os.system("lottie_convert.py ult.tgs json.json")
