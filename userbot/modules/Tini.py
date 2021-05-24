17385235572897a2aa9ad4cf3a2af5abdc84ae53
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-24 08:28:52 +0700

    Update Tini.py

--- userbot/modules/Tini.py
+++ userbot/modules/Tini.py
@@ -16,7 +16,7 @@ async def ultiny(event):
         return
     xx = await event.edit("`Memproses Tiny....`")
     ik = await bot.download_media(reply)
-    im1 = Image.open("userbot/AlphaUserbot.png")
+    im1 = Image.open("RAMUBOT.png")
     if ik.endswith(".tgs"):
         await event.client.download_media(reply, "ult.tgs")
         os.system("lottie_convert.py ult.tgs json.json")
