ef3ee1504a7f0a4e35ceeb169d113b8beda228b7
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-31 08:22:21 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -33,7 +33,7 @@ async def help(event):
         for i in CMD_HELP:
             string += "`" + str(i)
             string += "`\t ⌬  "
-        await event.edit("**😈 Alpha Userbot 😈**\n\n"
+        await event.edit("**⚙️ Alpha Userbot ⚙️**\n\n"
                          f"**◉ Bᴏᴛ ᴏꜰ {DEFAULTUSER}**\n**◉ Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
                          "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
                          f"◉ {string}◉\n\n")
