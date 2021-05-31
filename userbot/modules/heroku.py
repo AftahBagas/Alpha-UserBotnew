ce00a5f4ad6b074b61c35e08c587fc27d3a9b056
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-31 08:13:42 +0700

    Update heroku.py

--- userbot/modules/heroku.py
+++ userbot/modules/heroku.py
@@ -179,14 +179,14 @@ async def dyno_usage(dyno):
             AppMinutes = math.floor(AppQuotaUsed % 60)
 
             await dyno.edit(
-                "◆ **Ngecek Informasi Dyno** :\n┏────────────────────┓\n"
-                f"⁃ **Pemakaian Dyno {app.name} :**\n"
-                f"⁃ **Pemakaian** :  `{AppHours}` **Jam** - `{AppMinutes}` **Menit**\n"
-                f"⁃ **Persen** : `{AppPercentage}`**%**\n"
-                "▕──────────────────── \n"
-                f"⁃ **Sisa Dyno Ini Bulan :**\n"
-                f"⁃ **Sisa** :  `{hours}` **Jam** - `{minutes}` **Menit**\n"
-                f"⁃ **Persen** :  `{percentage}`**%**\n"
+                "⚙️ **Ngecek Informasi Dyno** :\n┏────────────────────┓\n"
+                f"⛽ **Pemakaian Dyno {app.name} :**\n"
+                f"➥ **Pemakaian** :  `{AppHours}` **Jam** - `{AppMinutes}` **Menit**\n"
+                f"➥ **Persen** : `{AppPercentage}`**%**\n"
+                "──────────────────── \n"
+                f"🛢️ **Sisa Dyno Ini Bulan :**\n"
+                f"➥ **Sisa** :  `{hours}` **Jam** - `{minutes}` **Menit**\n"
+                f"➥ **Persen** :  `{percentage}`**%**\n"
                 "┗────────────────────┛"
             )
             await asyncio.sleep(20)
