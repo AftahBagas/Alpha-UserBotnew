47916b2e1c36d9503f3219cce8ee6c9d5d2172ba
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-31 15:05:42 +0700

    Create adminswk.py

---
+++ userbot/modules/adminswk.py
@@ -0,0 +1,17 @@
+@register(outgoing=True, pattern=r"^\.admin$")
+async def get_admin(show):
+    info = await show.client.get_entity(show.chat_id)
+    title = info.title if info.title else "Grup Ini"
+    mentions = f"<b>🙈 Daftar Monyet Di Group {title}:</b> \n"
+    try:
+        async for user in show.client.iter_participants(
+            show.chat_id, filter=ChannelParticipantsAdmins
+        ):
+            if not user.deleted:
+                link = f'<a href="tg://user?id={user.id}">{user.first_name}</a>'
+                mentions += f"\n🙉 Monyet {link}"
+            else:
+                mentions += f"\nMonyet Meningoy <code>{user.id}</code>"
+    except ChatAdminRequiredError as err:
+        mentions += " " + str(err) + "\n"
+    await show.edit(mentions, parse_mode="html")
