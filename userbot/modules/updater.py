983126766b96c427e598d9edd4568589f7b3b6a7
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-03 13:46:54 +0700

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -194,7 +194,7 @@ async def upstream(event):
         return repo.__del__()
 
     if conf is None and force_update is False:
-        changelog_str = f'**⚙️ Pembaruan Untuk Alpha [{ac_br}]:\n\n📝 Pembaruan 📝:**\n`{changelog}`'
+        changelog_str = f'**⚙️ Pembaruan Untuk Alpha [{ac_br}]:\n\n📝 Pembaruan 📝**\n\n`{changelog}`'
         if len(changelog_str) > 4096:
             await event.edit("`Changelog Terlalu Besar, Lihat File Untuk Melihatnya.`")
             file = open("output.txt", "w+")
