62a0475cb5d05cdd36915e6e46d689dab2b1761a
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-06 21:13:29 +0700

    Update ping.py

--- userbot/modules/ping.py
+++ userbot/modules/ping.py
@@ -51,15 +51,7 @@ async def pingme(pong):
     """ For .ping command, ping the userbot from any chat.  """
     uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
-    await pong.edit("**.➠**")
-    await pong.edit("**..➠**")
-    await pong.edit("**...➠**")
-    await pong.edit("**....➠**")
-    await pong.edit("**.....➠**")
-    await pong.edit("**......➠**")
-    await pong.edit("**.......➠**")
-    await pong.edit("**PING**")
-    await pong.edit("**⚡**")
+    await pong.edit("**Pong!!**")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
     await pong.edit(f"**Alpha😈**\n"
