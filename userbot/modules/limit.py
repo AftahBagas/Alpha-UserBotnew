9f7a81abfacc99f47b9107891f66d1a0feb941c4
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-06 21:08:24 +0700

    Update limit.py

--- userbot/modules/limit.py
+++ userbot/modules/limit.py
@@ -1,16 +1,16 @@
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from userbot import CMD_HELP, USER_ID, bot
+from userbot import CMD_HELP, bot
 from userbot.events import register
 
 
 @register(outgoing=True, pattern='^.limit(?: |$)(.*)')
 async def demn(ult):
     chat = "@SpamBot"
-    msg = await event.edit("Checking If You Are Limited...")
+    msg = await event.edit("Memeriksa Apakah Anda Terbatas...")
     async with bot.conversation(chat) as conv:
         try:
             response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users={USER_ID})
+                events.NewMessage(incoming=True, from_users=178220800)
             )
             await conv.send_message("/start")
             response = await response
