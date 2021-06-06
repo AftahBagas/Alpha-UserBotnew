a7223c76227c167bddd4f32a2b833f1426487ca9
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-06 14:26:54 +0700

    Create alfahresah.py

--- userbot/modules/alfahresah.py
+++ userbot/modules/alfahresah.py
@@ -1,13 +1,21 @@
 from time import sleep
 from userbot.events import register
+from userbot import CMD_HELP, USER_AGE, ALIVE_NAME, COUNTRY
 
 
-@register(outgoing=True, pattern='^.alfareza(?: |$)(.*)')
+@register(outgoing=True, pattern='^.salken(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(3)
-    await typew.edit("`Hai Perkenalkan Namaku Alfareza`")
+    await typew.edit("`Hai Perkenalkan Namaku {ALIVE_NAME}`")
     sleep(3)
-    await typew.edit("`15 Tahun`")
+    await typew.edit("`Umurku {USER_AGE} Tahun`")
     sleep(1)
-    await typew.edit("`Tinggal Di Kota Pati, Salam Kenal:)`")
+    await typew.edit("`Tinggal Di {COUNTRY}, Salam Kenal:)`")
+
+
+CMD_HELP.update({
+    "salken":
+    "📚 **Cmd** : `.salken`\
+    \n📄 **Descriptions** : Command Salken Untuk Dirimu :)."
+})
