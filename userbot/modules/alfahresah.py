a42e006ad7ca9c378734ec3dfc94c2d79547d907
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-06 14:29:41 +0700

    Create alfahresah.py

--- userbot/modules/alfahresah.py
+++ userbot/modules/alfahresah.py
@@ -1,13 +1,13 @@
 from time import sleep
 from userbot.events import register
-from userbot import CMD_HELP
+from userbot import CMD_HELP, USER_AGE, ALIVE_NAME, COUNTRY
 
 
 @register(outgoing=True, pattern='^.salken(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(3)
-    await typew.edit("`Hai Perkenalkan Namaku {ALIVE_NAME}`")
+    await typew.edit("Hai Perkenalkan Namaku `{ALIVE_NAME}`")
     sleep(3)
     await typew.edit("`Umurku {USER_AGE} Tahun`")
     sleep(1)
