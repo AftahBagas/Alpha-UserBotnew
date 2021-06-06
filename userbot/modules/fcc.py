888bd72cbfce57cf5adb1e4dc7f8a58b3a216e03
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-06 21:58:03 +0700

    Create fcc.py

---
+++ userbot/modules/fcc.py
@@ -0,0 +1,26 @@
+
+from faker import Faker
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+
+from userbot import CMD_HELP
+from userbot.events import register
+
+
+@register(outgoing=True, pattern=r"^\.fcc(?: |$)(.*)")
+async def fcc(alfarezavent):
+    if geezevent.fwd_from:
+        return
+    alfarezacc = Faker()
+    alfarezaname = alfarezacc.name()
+    alfarezaadre = alfarezacc.address()
+    alfarezacard = alfarezacc.credit_card_full()
+
+    await edit_or_reply(alfarezaevent, f"__**🤵 Nama :- **__\n`{alfarezaname}`\n\n__**🏡 ADDRESS :- **__\n`{alfarezaadre}`\n\n__**💳 CARD :- **__\n`{alfarezacard}`")
+
+
+CMD_HELP.update({
+    "fcc":
+    "📚 **Cmd** : `.fcc`\
+    \n📄 **Descriptions** : mendapatkan informasi fake cc."
+})
