41381837e768866818e5326ff6b11ab9d0a2c044
AftahBagas <aftahbagas21@gmail.com>
2021-06-06 08:56:52 +0000

    auto back: code format
    Signed-off-by: AftahBagas <aftahbagas21@gmail.com>

--- userbot/modules/alfahresah.py
+++ userbot/modules/alfahresah.py
@@ -1,25 +1,7 @@
 """ Userbot module salken for deafult user"""
 
-from asyncio import sleep
-from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, bot, ALIVE_NAME, USER_AGE, COUNTRY
-from datetime import datetime
-from telethon import functions
-from emoji import emojize
-from math import sqrt
-from telethon.tl.functions.channels import GetFullChannelRequest, GetParticipantsRequest
-from telethon.tl.functions.messages import GetFullChatRequest, GetHistoryRequest
-from telethon.tl.types import MessageActionChannelMigrateFrom, ChannelParticipantsAdmins
-from telethon.errors import (
-    ChannelInvalidError,
-    ChannelPrivateError,
-    ChannelPublicGroupNaError)
-from telethon.utils import get_input_location
-from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantsBots
+from userbot import ALIVE_NAME, CMD_HELP, COUNTRY, USER_AGE
 from userbot.events import register
-from userbot.modules.admin import get_user_from_event
-from telethon.utils import pack_bot_file_id
-
-
 
 
 @register(outgoing=True, pattern="^.salken$")
@@ -30,7 +12,6 @@ async def salken(salken):
     await salken.edit(f"`Umurku {USER_AGE}`")
     await asyncio.sleep(2)
     await salken.edit(f"`Tinggal Di {COUNTRY} Salam Kenal :)`")
-
 
 
 CMD_HELP.update({
