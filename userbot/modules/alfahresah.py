859ce17eaff1c8a0eb91995dfada594b07e45ea1
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-06 15:30:59 +0700

    Create alfahresah.py

--- userbot/modules/alfahresah.py
+++ userbot/modules/alfahresah.py
@@ -1,8 +1,18 @@
 import asyncio
-from platform import uname
+from asyncio import create_subprocess_exec as asyncrunapp
+from asyncio.subprocess import PIPE as asyncPIPE
+from platform import python_version, uname
+from shutil import which
+from os import remove
+from telethon import version
+from telethon import __version__, version
+import platform
+import sys
 import time
+from datetime import datetime
+import psutil
 
-from userbot import ALIVE_NAME, CMD_HELP, StartTime, bot
+from userbot import ALIVE_LOGO, ALIVE_TEKS_KUSTOM, ALIVE_NAME, CMD_HELP, StartTime, UPSTREAM_REPO_BRANCH, bot
 from userbot.events import register
 
 
@@ -16,14 +26,15 @@ modules = CMD_HELP
 
 @ register(outgoing=True, pattern=r"^\.(?:salken)\s?(.)?")
 async def amireallysalken(salken):
-    await bot.get_me()
+    user = await bot.get_me()
     await get_readable_time((time.time() - StartTime))
-    await salken.edit("Halo Namaku ALIVE_NAME")
+    await salken.edit("Halo Semuanya :)")
     await asyncio.sleep(2)
-    await salken.edit("Umurku USER_AGE Tahun")
+    await salken.edit("Cuma Mau Bilang")
     await asyncio.sleep(2)
-    await salken.edit("Tinggal Di COUNTRY Salam Kenal :)")
-
+    output = (
+        f"`Namaku {DEFAULTUSER} Tinggal Di {USER_AGE}`\n"
+        f"`Salam Kenal :)`")
 
 CMD_HELP.update({
     "salken":
