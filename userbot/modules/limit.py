0109db4ed655b36b6ce361cb1a2a37c760e53556
AftahBagas <aftahbagas21@gmail.com>
2021-06-06 06:49:32 +0000

    auto back: code format
    Signed-off-by: AftahBagas <aftahbagas21@gmail.com>

--- userbot/modules/limit.py
+++ userbot/modules/limit.py
@@ -3,17 +3,17 @@ from userbot import CMD_HELP, bot
 from userbot.events import register
 
 
-@register(outgoing=True, pattern=r'^.limit(?: |$")
+@register(outgoing=True, pattern=r'^ .limit(?: |$")
 async def demn(ult):
-    chat = "@SpamBot"
-    msg = await event.edit("Checking If You Are Limited...")
+    chat="@SpamBot"
+    msg=await event.edit("Checking If You Are Limited...")
     async with bot.conversation(chat) as conv:
         try:
-            response = conv.wait_event(
+            response=conv.wait_event(
                 events.NewMessage(incoming=True, from_users=1743866353)
             )
             await conv.send_message("/start")
-            response = await response
+            response=await response
             await bot.send_read_acknowledge(chat)
         except YouBlockedUserError:
             await steal.reply("Alpha User! Please Unblock @SpamBot ")
@@ -24,5 +24,5 @@ async def demn(ult):
 CMD_HELP.update({
     "limited":
     "📚 **Cmd** : `.limit`\
-\n📄 **Descriptions** : Mengecek limit
+\n📄 **Descriptions**: Mengecek limit
 })
