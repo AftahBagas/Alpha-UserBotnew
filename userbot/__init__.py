d048045db492eb2f1c17fc3dba7fc3ab00df3c1b
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-06 22:53:50 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -166,6 +166,9 @@ ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
 # Custom Emoji Alive
 ALIVE_EMOJI = os.environ.get("ALIVE_EMOJI", "😈")
 
+# Custom Emoji Cmd .helpme
+HELP_EMOJI = os.environ.get("HELP_EMOJI", "🔸")
+
 # Time & Date - Country and Time Zone
 COUNTRY = str(os.environ.get("COUNTRY", "ID"))
 TZ_NUMBER = int(os.environ.get("TZ_NUMBER", 1))
@@ -365,6 +368,7 @@ CMD_HELP = {}
 ISAFK = False
 AFKREASON = None
 ZALG_LIST = {}
+HELP_EMOJI = 🔸
 
 
 def paginate_help(page_number, loaded_modules, prefix):
@@ -373,7 +377,7 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline("{} {}".format("🔸", x), data="ub_modul_{}".format(x))
+        custom.Button.inline("{} {}".format("{HELP_EMOJI}", x), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
