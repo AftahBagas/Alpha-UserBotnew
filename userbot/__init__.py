91f8602e8002c0f49aa97241528854e6c0b271b4
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-06 23:06:19 +0700

    Create __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -368,7 +368,6 @@ CMD_HELP = {}
 ISAFK = False
 AFKREASON = None
 ZALG_LIST = {}
-HELP_EMOJI = 🔸
 
 
 def paginate_help(page_number, loaded_modules, prefix):
@@ -377,7 +376,7 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline("{} {}".format("await salken.edit(f"`Umurku {USER_AGE}`") ", x), data="ub_modul_{}".format(x))
+        custom.Button.inline("{} {}".format("await event.reply(f"`Hai {ALIVE_NAME}\n\nApa Kabarmu?`")",x),data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
