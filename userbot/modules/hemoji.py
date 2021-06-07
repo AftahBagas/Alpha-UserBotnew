62803495a8bea4e2068d2cc1529e171078c0bcd4
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-07 10:29:42 +0700

    Update hemoji.py

--- userbot/modules/hemoji.py
+++ userbot/modules/hemoji.py
@@ -3,5 +3,6 @@ from userbot import CMD_HELP
 
 CMD_HELP.update({
     "hemoji":
-    "📄 **Descriptions** : Ketik .set var HELP_EMOJI {emoji bebas} untuk tampilan emoji Cmd .helpme mu :)."
+    "📚 **Cmd** : `Baca Descriptions`\
+    \n"📄 **Descriptions** : Ketik .set var HELP_EMOJI {emoji bebas} untuk tampilan emoji Cmd .helpme mu :)."
 })
