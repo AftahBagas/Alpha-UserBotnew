a865b4312ace4b93898a26ad139e5f2ff7f700ef
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-07 14:54:23 +0700

    Update hemoji.py

--- userbot/modules/hemoji.py
+++ userbot/modules/hemoji.py
@@ -4,5 +4,5 @@ from userbot import CMD_HELP
 CMD_HELP.update({
     "hemoji":
         "📚 **Cmd** : `lihat Descriptions`\
-    \n📄 **Descriptions** : Ketik .set var HELP_EMOJI (emoji bebas yg kamu mau) untuk emoji Cmd helpmemu."
+    \n📄 **Descriptions** : Ketik .set var HELP_EMOJI (emoji bebas yg kamu mau) untuk emoji Command .helpme mu."
 })
