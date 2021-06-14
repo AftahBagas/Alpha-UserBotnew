db3f8d33f53e6fc97bcfea1f43db9e662140da9c
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-14 09:03:41 +0700

    Update limit.py

--- userbot/modules/limit.py
+++ userbot/modules/limit.py
@@ -1,24 +1,25 @@
+from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from userbot import CMD_HELP, bot
+
+from userbot import bot, CMD_HELP
 from userbot.events import register
 
 
-@register(outgoing=True, pattern='^.limit(?: |$)(.*)')
-async def demn(ult):
-    chat = "@SpamBot"
-    msg = await event.edit("Memeriksa Apakah Anda Terbatas...")
-    async with bot.conversation(chat) as conv:
+@register(outgoing=True, pattern=r"^\.limit(?: |$)(.*)")
+async def _(event):
+    await event.edit("`Checking If You Are Limited...`")
+    async with bot.conversation("@SpamBot") as conv:
         try:
             response = conv.wait_event(
                 events.NewMessage(incoming=True, from_users=178220800)
             )
             await conv.send_message("/start")
             response = await response
-            await bot.send_read_acknowledge(chat)
+            await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
-            await steal.reply("Alpha User! Please Unblock @SpamBot ")
+            await event.edit("`Alpha User! Please Unblock @SpamBot`")
             return
-        await msg.edit(f"~ {response.message.message}")
+        await event.edit(f"~ {response.message.message}")
 
 
 CMD_HELP.update({
