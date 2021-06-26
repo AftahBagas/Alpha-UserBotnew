cbe106259be18a4bb2c7e66a0e83ed9cd0c4fe27
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-26 14:07:59 +0700

    Create __helpmy.py

---
+++ userbot/modules/__helpmy.py
@@ -0,0 +1,27 @@
+
+from userbot import BOT_USERNAME
+from userbot.events import register
+
+# ██████ LANGUAGE CONSTANTS ██████ #
+
+from userbot.language import get_value
+LANG = get_value("__helpme")
+
+# ████████████████████████████████ #
+
+@register(outgoing=True, pattern="^.helpmy")
+async def yardim(event):
+    tgbotusername = BOT_USERNAME
+    if tgbotusername is not None:
+        results = await event.client.inline_query(
+            tgbotusername,
+            f"{BOT_USERNAME}"
+        )
+        await results[0].click(
+            event.chat_id,
+            reply_to=event.reply_to_msg_id,
+            hide_via=True
+        )
+        await event.delete()
+    else:
+        await event.edit(LANG["NO_BOT"])
