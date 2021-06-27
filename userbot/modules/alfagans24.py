a01c8020aa6107e324c938620edee4926e5f859f
AftahBagas <aftahbagas21@gmail.com>
2021-06-27 04:29:25 +0000

    auto back: code format
    Signed-off-by: AftahBagas <aftahbagas21@gmail.com>

--- userbot/modules/alfagans24.py
+++ userbot/modules/alfagans24.py
@@ -1,7 +1,7 @@
 # p
 
 from time import sleep
-from userbot import CMD_HELP, bot
+from userbot import bot
 from userbot.events import alphabot
 from userbot.cmdhelp import CmdHelp
 from telethon import events
