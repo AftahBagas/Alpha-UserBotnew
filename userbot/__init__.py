2c259199ec8478b9f17b6fbbbea1a8061b75492d
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-27 11:23:16 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -1,8 +1,5 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-# inline credit @keselekpermen69
+# Alfareza
+# inline credit @aftahbagas
 # Pengguna Alpha Userbot
 """ Userbot initialization. """
 
