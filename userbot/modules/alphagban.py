5fe5ec5d94a6e87dcfa382bde436adece0fa551b
AftahBagas <aftahbagas21@gmail.com>
2021-06-27 06:36:12 +0000

    auto back: code format
    Signed-off-by: AftahBagas <aftahbagas21@gmail.com>

--- userbot/modules/alphagban.py
+++ userbot/modules/alphagban.py
@@ -2,7 +2,7 @@
 # alfareza
 
 from telethon.events import ChatAction
-from userbot import ALIVE_NAME, CMD_HELP, bot
+from userbot import ALIVE_NAME, bot
 from telethon.tl.functions.contacts import BlockRequest, UnblockRequest
 from userbot.events import alphabot
 from telethon.tl.types import MessageEntityMentionName
